068a1bffc796ff802d58d20509f9d78d1cf42941
heinrich26 <65970327+heinrich26@users.noreply.github.com>
2021-05-30 11:28:38 +0200

    major changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 from main_ui import Ui_MainWindow
 from tkinter import filedialog
-import pygame, sys, os
+import pygame, sys, os, random, threading, time
 from pathlib import Path
+from mutagen.mp3 import MP3, EasyID3
 
 
 programm_path = os.path.abspath(os.path.dirname(sys.argv[0]))
@@ -12,40 +13,111 @@ programm_path = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 lastdir = str(Path.home()) + "/Music/"
 
-playing = False
-
-def play_pause(self):
-	global playing
-	if playing:
-		pygame.mixer.pause()
-		playing = False
-	else:
-		selected = self.ui.PlaylistContainer.CurrentRow()
-
-		pygame.mixer.load(self.songpaths[selected])
-		pygame.mixer.play(loops=0)
-		playing = True
 
 
 class MainWindow(QtWidgets.QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
+		self.songlength = -0.001
+		self.playing = False
 
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
+
+		pygame.mixer.init()
+		self.is_alive = True
+		self.tracktime_thread = threading.Thread(target=self.tracktime_updater)
+		self.tracktime_thread.start()
+
+
+		# setup the connections
+		self.ui.actionAddSong.triggered.connect(self.add_song)
+		self.ui.actionPlay_Pause.toggled["bool"].connect(self.play_pause)
+		#self.ui.PlaylistContainer.itemClicked['QListWidgetItem*'].connect(self.play_pause)
 		self.songlist = []
-		self.sonpaths = []
+		self.songdata = {}
+
+	def closeEvent(self, event):
+		self.is_alive = False
+		event.accept()
+
+	def tracktime_updater(self):
+		while self.is_alive:
+			if self.playing:
+				self.ui.TrackTime.setProperty("value", round((float(pygame.mixer.music.get_pos())/(self.songlength*1000))*1000))
+			time.sleep(0.25)
 
 	def add_song(self):
 		global lastdir
-		files = filedialog.askopenfilenames(initialdir=lastdir, title="Select Files", filetypes=(("Audio Files", "mp3")))
-		for file in files:
-			song = self.PlaylistContainer.item(len(self.songlist))
-			song.setText("1st",
-"2nd")
-			self.songlist.append(song)
-			self.songpaths.append(file.replace("\\", "/"))
+		options = QtWidgets.QFileDialog.Options()
+		files, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"Select Song(s)", lastdir, "Audio Files (*.mp3)", options=options)
 
+		font = QtGui.QFont()
+		font.setBold(True)
+		font.setWeight(75)
+		default_icon = QtGui.QIcon()
+		default_icon.addPixmap(QtGui.QPixmap("../../../Pictures/2colorthing.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
+		files = [file.replace("\\", "/") for file in files]
+
+		for file in files with EasyID3(file) as sondobj:
+			# song = QtWidgets.QListWidgetItem()
+			# song.setFont(font)
+			# if False:
+			# 	print("oups")
+			# else:
+			# 	song.setIcon(default_icon)
+			# song.setText("1st\n2nd")
+			# self.ui.PlaylistContainer.addItem(song)
+			self.songlist.append(song)
+			self.songdata[file] = {"album": songobj["album"], "title": songobj["title"], "artist": songobj["artist"], "length":songobj["length"]}
+
+	def add_playlist_item(self, song):
+
+
+	def play_pause(self, arg):
+		if type(arg) == bool:
+			if not arg:
+				pygame.mixer.music.pause()
+				self.playing = False
+			elif pygame.mixer.music.get_pos() == -1:
+				new_song = self.songpaths[random.randint(0,len(self.songpaths)-1)]
+
+				info_song = MP3(new_song)
+
+				pygame.mixer.music.load(new_song)
+				self.current_song = new_song
+				self.songlength = info_song.info.length
+
+				pygame.mixer.music.play(loops=0)
+				self.playing = True
+				del info_song
+			else:
+				pygame.mixer.music.unpause()
+				self.playing = True
+		else:
+			selected = self.ui.PlaylistContainer.currentRow()
+
+			try:
+				old_song = copy(current_song)
+			except:
+				pass
+			new_song = self.songpaths[selected]
+
+			info_song = MP3(new_song)
+
+			pygame.mixer.music.load(new_song)
+			self.current_song = new_song
+			self.songlength = info_song.info.length
+
+			pygame.mixer.music.play(loops=0)
+			self.playing = True
+			self.ui.actionPlay_Pause.setChecked(True)
+			del info_song
+			try:
+				pygame.mixer.music.unload(old_song)
+			except:
+				pass
 
 
 
@@ -53,8 +125,5 @@ if __name__ == "__main__":
 	app = QtWidgets.QApplication(sys.argv)
 	player = MainWindow()
 	player.show()
-
-	pygame.mixer.init()
-	player.add_song()
 
 sys.exit(app.exec_())
---
+++ track.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# self implementation generated from reading ui file 'track.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class PlaylistItem(object):
+    def __init__(self, file):
+
+        self.setWindowModality(QtCore.Qt.NonModal)
+        self.setEnabled(True)
+        self.resize(392, 76)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMaximumSize(QtCore.QSize(16777215, 166))
+        self.setStatusTip("")
+        self.setWhatsThis("")
+        self.setAccessibleName("")
+        self.setAccessibleDescription("")
+        self.setWindowFilePath("")
+        self.gridLayout = QtWidgets.QGridLayout(self)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setSpacing(4)
+        self.gridLayout.setObjectName("gridLayout")
+        self.TrackInfoBox = QtWidgets.QWidget(self)
+        self.TrackInfoBox.setObjectName("TrackInfoBox")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.TrackInfoBox)
+        self.verticalLayout.setContentsMargins(1, 8, 0, 8)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.TrackInfo = QtWidgets.QLabel(self.TrackInfoBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.TrackInfo.sizePolicy().hasHeightForWidth())
+        self.TrackInfo.setSizePolicy(sizePolicy)
+        self.TrackInfo.setMaximumSize(QtCore.QSize(16777215, 40))
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift SemiBold Condensed")
+        font.setPointSize(22)
+        font.setBold(True)
+        font.setWeight(75)
+        self.TrackInfo.setFont(font)
+        self.TrackInfo.setStyleSheet("QLabel {\n"
+"    line-height:5;\n"
+"}")
+        self.TrackInfo.setText("First")
+        self.TrackInfo.setScaledContents(False)
+        self.TrackInfo.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.TrackInfo.setWordWrap(False)
+        self.TrackInfo.setIndent(2)
+        self.TrackInfo.setObjectName("TrackInfo")
+        self.verticalLayout.addWidget(self.TrackInfo)
+        self.TrackInfoSub = QtWidgets.QLabel(self.TrackInfoBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.TrackInfoSub.sizePolicy().hasHeightForWidth())
+        self.TrackInfoSub.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift SemiBold Condensed")
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setKerning(True)
+        self.TrackInfoSub.setFont(font)
+        self.TrackInfoSub.setStyleSheet("color:#707070")
+        self.TrackInfoSub.setText("Second")
+        self.TrackInfoSub.setScaledContents(False)
+        self.TrackInfoSub.setIndent(3)
+        self.TrackInfoSub.setObjectName("TrackInfoSub")
+        self.verticalLayout.addWidget(self.TrackInfoSub)
+        self.gridLayout.addWidget(self.TrackInfoBox, 0, 1, 1, 1)
+        self.TrackCover = QtWidgets.QLabel(self)
+        self.TrackCover.setMaximumSize(QtCore.QSize(76, 76))
+        self.TrackCover.setAutoFillBackground(False)
+        self.TrackCover.setStyleSheet("QLabel {\n"
+"    background-color: #000;\n"
+"    margin: 8px\n"
+"}")
+        self.TrackCover.setText("")
+        self.TrackCover.setPixmap(QtGui.QPixmap())
+        self.TrackCover.setScaledContents(True)
+        self.TrackCover.setAlignment(QtCore.Qt.AlignCenter)
+        self.TrackCover.setWordWrap(False)
+        self.TrackCover.setObjectName("TrackCover")
+        self.gridLayout.addWidget(self.TrackCover, 0, 0, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self)
+        self.pushButton.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/src/svg/src/svg/dropdown.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton.setFlat(True)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 2, 1, 1)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+import resources_rc
